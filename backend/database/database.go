993f5ec6ee9b1ba8bc42c6e60af31c948f716dbe
cashev <42924021+cashev@users.noreply.github.com>
2024-10-14 00:26:25 +0900

    herokuでデプロイするためにDATABASE_URLから接続

--- backend/database/database.go
+++ backend/database/database.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/lib/pq"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,20 +13,15 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
-		host,
-		user,
-		password,
-		dbname,
-		port,
-	)
-	fmt.Println(dsn)
-
+	url := os.Getenv("DATABASE_URL")
+	if url == "" {
+		panic("環増変数 DATABASE_URL が設定されていません")
+	}
+	connection, err := pq.ParseURL(url)
+	if err != nil {
+		panic("データベース接続情報の解析に失敗しました: " + err.Error())
+	}
+	dsn := fmt.Sprintf("%s sslmode=disable TimeZone=Asia/Tokyo", connection)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("データベース接続に失敗しました: " + err.Error())
