3427fbd17308a75a8ef3a8a3569a34093232bb03
cashev <42924021+cashev@users.noreply.github.com>
2024-10-31 01:17:27 +0900

    fix not export authOptions from route.ts

--- frontend/src/app/student/tasks/page.tsx
+++ frontend/src/app/student/tasks/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/app/api/auth/[...nextauth]/options'
 import { getTasksByStudent } from '@/lib/backend/task'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
